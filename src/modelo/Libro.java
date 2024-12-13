bef8ecf6cfd2ec6cff4b7ae4da969a66dbc626f8
JovenVargas <isaac.vargas768@ist17dejulio.edu.ec>
2024-12-13 13:35:06 -0500

    Parte 2

--- src/modelo/Libro.java
+++ src/modelo/Libro.java
@@ -9,6 +9,93 @@ package modelo;
  * @author Tiririn
  */
 public class Libro implements Disponibilidad {
-   
+
+    private int id;
+    private  int isbn;
+    private String  titulo;
+    private String autor;
+    private boolean disponible;
+    
+
+    public Libro() {
+        this.disponible = true; 
+    }
+
+    public Libro(int id, int isbn, String titulo, String autor, boolean disponible) {
+        this.id = id;
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = disponible;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+     @Override
+    public String toString() {
+        return "ID: " + id + "\nISBN: " + isbn + "\nTítulo: " + titulo + "\nAutor: " + autor + "\nEstado: " + (disponible ? "Disponible" : "No Disponible");
+    }
+
+   @Override
+    public boolean disponibilidad() {
+        return disponible; 
+    }
+
+    @Override
+    public void marcarDisponible() {
+        this.disponible = true; 
+    }
+
+    @Override
+    public void marcarNoDisponible() {
+        this.disponible = false; 
+    }
+
+
+    
+    
 }
+
+
+
+
+
     
